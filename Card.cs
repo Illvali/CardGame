e787c0cb92f0c9057e3da453de42f273649a9b04
Ylva Blomkvist <48473765+Illvali@users.noreply.github.com>
2021-03-19 08:11:20 +0100

    Update Card.cs

--- Card.cs
+++ Card.cs
@@ -3,32 +3,29 @@
 
 namespace Cardgame
 {
-    partial class Program
+    class Card
     {
-        class Card
+        public string Color { get; private set; }
+        public int Value { get; private set; }
+        public string ShowValue { get; private set; }
+        public bool Royal { get; private set; }
+        public Card(string color, int value, string showValue)
         {
-            public string Color { get; private set; }
-            public int Value { get; private set; }
-            public string ShowValue { get; private set; }
-            public bool Royal { get; private set; }
-            public Card(string color, int value, string showValue)
+            Color = color;
+            Value = value;
+            ShowValue = showValue;
+            if(Value > 8)
             {
-                Color = color;
-                Value = value;
-                ShowValue = showValue;
-                if(Value > 8)
-                {
-                    Royal = true;
-                }
-                else
-                {
-                    Royal = false;
-                }
+                Royal = true;
             }
-            public void PresentCard()
+            else
             {
-                Console.WriteLine($"Card: {ShowValue} of {Color}s");
+                Royal = false;
             }
+        }
+        public void PresentCard()
+        {
+            Console.WriteLine($"Card: {ShowValue} of {Color}s");
         }
     }
 }
